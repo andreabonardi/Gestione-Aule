0bb89c8718414306475be91f504fdddafdd0fa8a
andreabonardi <abona2000@hotmail.it>
2021-03-02 15:23:04 +0100

    generate component corsi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuleComponent } from './aule/aule.component';
 import { StudentiComponent } from './studenti/studenti.component';
+import { CorsiComponent } from './corsi/corsi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     AuleComponent,
-    StudentiComponent
+    StudentiComponent,
+    CorsiComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/corsi/corsi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-corsi',
+  templateUrl: './corsi.component.html',
+  styleUrls: ['./corsi.component.css']
+})
+export class CorsiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
